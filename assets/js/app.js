119ff7698254fa361943cbc2fa4bc21e21ee496a
CEGonzalezRojas <claudioesteban.gonzalezrojas@gmail.com>
2022-10-02 17:24:35 -0300

    Agregado glide.js y eliminado slick por uso de jquery

--- assets/js/app.js
+++ assets/js/app.js
@@ -43,29 +43,15 @@ import LazyLoad from "./lazyLoad.js";
      * Genera el carrusel para las marcas
      */
     BrandCarousel(){
-      $(document).ready(function() {
-        $('.logo-carousel').slick({
-          slidesToShow: 6,
-          slidesToScroll: 1,
-          autoplay: true,
-          autoplaySpeed: 1000,
-          arrows: true,
-          arrowsLabel: false,
-          dots: false,
-          pauseOnHover: false,
-          responsive: [{
-            breakpoint: 768,
-            settings: {
-              slidesToShow: 4
-            }
-          }, {
-            breakpoint: 520,
-            settings: {
-              slidesToShow: 2
-            }
-          }]
-        });
-      });
+
+      new Glide('.glide', {
+        type: 'carousel',
+        perView: 6,
+        autoplay: 2000,
+        gap: 50,
+        hoverpause: true
+      }).mount();
+
     }
   }
 
